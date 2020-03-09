076bbb14d5d8741d2b3911be3bb6dd88c13b49cc
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-03-08 20:31:52 -0600

    updated the bootstrap to match

--- server.js
+++ server.js
@@ -15,57 +15,54 @@ app.use(express.static(__dirname + "/public"));
 //setting up routes
 
 //returns for the html 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 // pull from db.json
-app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function (req, res) {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
-      console.log(data);
-      if (err) throw err;
-      const note = JSON.parse(data);
-      return res.json(note);
+        console.log(data);
+        if (err) throw err;
+        const note = JSON.parse(data);
+        return res.json(note);
     });
-  });
-  
-  // add to db.json
-  app.post("/api/notes", function(req, res) {
+});
+
+// add to db.json
+app.post("/api/notes", function (req, res) {
     data.push(req.body);
-    fs.writeFile("./db/db.json", JSON.stringify(data), function(err) {
-      if (err) throw err;
-      console.log("New post successful!");
+    fs.writeFile("./db/db.json", JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log("New post successful!");
     });
     return res.json(data);
-  });
+});
 
-  // note to delete
-app.delete("/api/notes/:id", function(req, res) {
-    data = data.filter(function(data) {
-      if (req.params.id === data.id) {
-        return false;
-      }
-      return true;
+// note to delete
+app.delete("/api/notes/:id", function (req, res) {
+    data = data.filter(function (data) {
+        if (req.params.id === data.id) {
+            return false;
+        }
+        return true;
     });
-  
-    fs.writeFile("./db/db.json", JSON.stringify(data), function(err) {
-      if (err) throw err;
-      res.end();
+
+    fs.writeFile("./db/db.json", JSON.stringify(data), function (err) {
+        if (err) throw err;
+        res.end();
     });
-  
+
     return res.json(data);
-  });
-  
-  //server begin listening code: 
+});
 
-  app.listen(PORT, function() {
+//server begin listening code: 
+
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
-
-
-
+});
 
