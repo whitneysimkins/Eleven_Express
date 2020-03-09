abd352482bdf5ec7c4b679844543041b2526866a
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-03-08 20:14:01 -0600

    routes for the html

--- server.js
+++ server.js
@@ -8,5 +8,21 @@ var PORT = process.env.PORT || 3000;
 let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+//setting up routes
+
+//returns for the html 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+
 
 
