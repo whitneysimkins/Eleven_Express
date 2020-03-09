5ea2482fea1f4acbd6d8a5e6ca15fa6b33ce6f19
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-03-08 20:01:40 -0600

    structuring files and adding html

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-//setting up variables needed for app
+//setting up dependencies needed for app created package-lock.json
 var express = require("express");
 var path = require("path");
-var fs = require("fs")
+var fs = require("fs");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+
 
 
