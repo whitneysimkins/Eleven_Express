795293a28d497d8ac4a4378bad76af6eb7163a5f
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-03-11 19:44:44 -0600

    heroku problems

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //setting up dependencies needed for app created package-lock.json
-var express = require("express");
+var app = require("express");
 var path = require("path");
 var fs = require("fs");
 
